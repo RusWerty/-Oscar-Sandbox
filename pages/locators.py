162db84c1f6b7bc00ea73b9c50245d00ac379d05
RusWerty <ruslan.ibeneyev@gmail.com>
2025-07-11 10:07:25 +0500

    Добавлены локаторы и проверки для страницы логина

--- pages/locators.py
+++ pages/locators.py
@@ -5,4 +5,16 @@ class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 class LoginPageLocators():
-    
+    LOGIN_FORM = (By.CSS_SELECTOR, "form#login_form.well")
+    REGISTER_FORM = (By.CSS_SELECTOR, "form#register_form.well")
+
+    # Поля логина
+    LOGIN_EMAIL_INPUT = (By.CSS_SELECTOR, "#login_form input#id_login-username")
+    LOGIN_PASSWORD_INPUT = (By.CSS_SELECTOR, "#login_form input#id_login-password")
+    LOGIN_SUBMIT_BUTTON = (By.CSS_SELECTOR, "#login_form button[name='login_submit']")
+
+    # Поля регистрации
+    REGISTER_EMAIL_INPUT = (By.CSS_SELECTOR, "#register_form input#id_registration-email")
+    REGISTER_PASSWORD1_INPUT = (By.CSS_SELECTOR, "#register_form input#id_registration-password1")
+    REGISTER_PASSWORD2_INPUT = (By.CSS_SELECTOR, "#register_form input#id_registration-password2")
+    REGISTER_SUBMIT_BUTTON = (By.CSS_SELECTOR, "#register_form button[name='registration_submit']")
